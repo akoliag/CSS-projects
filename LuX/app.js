b9c8eeef5a0aaed4fb9a515f5fbaa8e6f130b817
Aga <a.kolia.grudzien@gmail.com>
2019-02-11 20:06:34 +0100

    navbar fix

---
+++ LuX/app.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.menu-toggle').click(function () {
+        $('.site-nav').toggleClass('site-nav--open', 500);
+        $(this).toggleClass('open');
+    });
+});
+
+$(document).on('click', 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 600);
+});
