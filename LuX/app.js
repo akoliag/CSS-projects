a9dfbdf10973e5e9885a260944aea73d5f02ef3b
Aga <a.kolia.grudzien@gmail.com>
2019-02-17 18:16:03 +0100

    add script at the end

--- LuX/app.js
+++ LuX/app.js
@@ -11,21 +11,19 @@ $(function() {
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 600);
   });
-
-  let slideIndex = 0;
-  slideshow();
-
-  function slideshow() {
-    let i;
-    let x = document.getElementsByClassName('mySlides');
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = 'none';
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {
-      slideIndex = 1;
-    }
-    x[slideIndex - 1].style.display = 'block';
-    setTimeout(slideshow, 6000);
-  }
 });
+
+let slideIndex = 0;
+slideshow();
+
+function slideshow() {
+  let i;
+  const x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(slideshow, 4000);
+}
