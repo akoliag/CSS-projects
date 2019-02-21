e4b20849e1f82bfd0adcf4a4133b053d03d47271
Aga <a.kolia.grudzien@gmail.com>
2019-02-21 20:21:39 +0100

    manually clicked slideshow

--- LuX/app.js
+++ LuX/app.js
@@ -13,17 +13,35 @@ $(function() {
   });
 });
 
-let slideIndex = 0;
-slideshow();
+// let slideIndex = 0;
+// slideshow();
+//
+// function slideshow() {
+//   let i;
+//   const x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(slideshow, 10000);
+// }
 
-function slideshow() {
-  let i;
-  const x = document.getElementsByClassName("mySlides");
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(slideshow, 8000);
 }
